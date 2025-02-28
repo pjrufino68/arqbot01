462e4afa95054c30dd4125db8900642ae20611c7
pjrufino68 <pjfernandesrufino@gmail.com>
2025-02-28 10:11:14 -0300

    Add files via upload

--- botAI.py
+++ botAI.py
@@ -33,35 +33,28 @@ Caso seja feita alguma pergunta fora do contexto, responda que você tem conheci
 """
 }
 
-resposta = ""
 listaMensagens = []
 listaMensagens.insert(0, assistant_instructions)
 
+st.session_state["messages"] = listaMensagens
+
 @st.cache_data
 
-def enviarMensagem(mensagem, listaMensagens=[]):
-    listaMensagens.append(
-        {"role": "user", "content": mensagem}    
-    )
-    resposta = client.chat.completions.create( 
-    #resposta = openai.ChatCompletion.create(
-        model = "gpt-4o-mini",
-        messages = listaMensagens
-    )
-    return resposta.choices[0].message.content
-    #return resposta["choices"][0]["message"]
+def montarIA(textoRecebido):
+    st.session_state.messages.append({"role": "user", "content": texto})
+    response = client.chat.completions.create(model="gpt-4o-mini", messages=st.session_state.messages)
+    return response.choices[0].message.content    
 
 with st.container():
     i = 0
     while True:
-        texto = st.text_input("Digite sua pergunta ou fim para encerrar: ", key=i)
+        texto = st.text_input("Faça sua pergunta: ", key=i)
         i = i + 1
         if texto == "fim" or len(texto) < 1:
             break
         else:
-            resposta = enviarMensagem(texto, listaMensagens)
-            listaMensagens.append(resposta)
-            st.write("ArqBot: ", resposta)
+            msg = montarIA(texto)
+            st.chat_message("assistant").write(msg)
 
     if texto == "fim":
         st.write("ArqBot: Até mais! ArqBot à disposição!")
@@ -69,6 +62,8 @@ with st.container():
 exit_app = st.button("Finalizar")
 if exit_app:
     keyboard.press_and_release('ctrl+w')
+    #st.stop()
     pid = os.getpid()
     p = psutil.Process(pid)
     p.terminate()
+
