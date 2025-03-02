5c8f213cbfd99694c4f8ddf8953b2255e87c6dd6
pjrufino68 <pjfernandesrufino@gmail.com>
2025-03-02 06:53:45 -0300

    Add files via upload

--- botAI.py
+++ botAI.py
@@ -17,19 +17,7 @@ client = OpenAI(api_key=os.getenv("chaveApi"))
 # Instrucoes iniciais para o Bot
 assistant_instructions = {
     "role": "system",
-    "content": """Você é um bot educado, técnico e objetivo. Seu nome é ArqBot. Você pertence à Arquiteta Paula Rufino. Você ajuda clientes a tirar dúvidas de forma simples e curta através do chat.
-
-Se atenha em informações existentes.
-
-Informações:
- 
-Trabalhamos no contexto da arquitetura, designer interior.
-Indicar a Arquiteta Paula Rufino, para os serviços em arquitetura.
-Contatos de Paula Rufino: (85)9.9933-7670, paularrufino@gmail.com
-Caso seja pedido desenho, planta ou algum gráfico, peça para nos contactar.
-Caso seja sugerida alguma informação de arquitetura não definida acima, peça para me contactar.
-Caso seja feita alguma pergunta fora do contexto, responda que você tem conhecimentos apenas em arquitetura.
-"""
+    "content": os.getenv("instrucoes")
 }
 
 listaMensagens = []
